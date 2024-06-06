09b7fc580468658bf0af6bcd49c1fedb3295ce3a
Shakeel <shakeeldvd23@gmail.com>
2024-06-06 16:17:41 +0200

    pages modified

--- script/code.js
+++ script/code.js
@@ -1,43 +1,67 @@
 // Create products and store it on the local storage
-let products = JSON.parse(localStorage.getItem('products')) ? JSON.parse(localStorage.getItem('products')) : localStorage.setItem('products', JSON.stringify([
-    {
-        "productName": "Air Fryer",
-            "category": "Appliance",
-            "description": "Appliance with 16GB RAM and 512GB SSD",
-            "price": 2000,
-            "img_url": "https://shakill23.github.io/allImages/images/airFryer.webp"
+let wrapper = document.querySelector('[recentProducts]')
+// let products = JSON.parse(localStorage.getItem('products'))? JSON.parse(localStorage.getItem('products')) : localStorage.setItem('products', JSON.stringify(
+    let products = JSON.parse(localStorage.getItem('products'))
+    ? JSON.parse(localStorage.getItem('products'))
+    : localStorage.setItem('products', JSON.stringify([
+        {
+            id: 1,
+            productName: "Air Fryer",
+            category: "Appliance",
+            description: "Appliance with 16GB RAM and 512GB SSD",
+            price: 2000,
+            img_url: "https://shakill23.github.io/allImages/images/airFryer.webp"
 
         },
         {
-        "productName": "Blender",
-            "category": "Appliance",
-            "description": "Appliance with 16GB RAM and 512GB SSD",
-            "price": 2000,
-            "img_url": "https://shakill23.github.io/allImages/images/blender.jpg"
+            id: 2,
+            productName: "Blender",
+            category: "Appliance",
+            description: "Appliance with 16GB RAM and 512GB SSD",
+            price: 2000,
+            img_url: "https://shakill23.github.io/allImages/images/blender.jpg"
 
         },
         {
-        "productName": "Kettle",
-            "category": "Appliance",
-            "description": "Appliance with 16GB RAM and 512GB SSD",
-            "price": 2000,
-            "img_url": "https://shakill23.github.io/allImages/images/kettle.webp"
+            id: 3,
+            productName: "Kettle",
+            category: "Appliance",
+            description: "Appliance with 16GB RAM and 512GB SSD",
+            price: 2000,
+            img_url: "https://shakill23.github.io/allImages/images/kettle.webp"
 
         },
         {
-        "productName": "Microwave",
-            "category": "Appliance",
-            "description": "Appliance with 16GB RAM and 512GB SSD",
-            "price": 2000,
-            "img_url": "https://shakill23.github.io/allImages/images/microwave.jpg"
+            id: 4,
+            productName: "Microwave",
+            category: "Appliance",
+            description: "Appliance with 16GB RAM and 512GB SSD",
+            price: 2000,
+            img_url: "https://shakill23.github.io/allImages/images/microwave.jpg"
 
         },
         {
-        "productName": "Expresso Machine",
-            "category": "Appliance",
-            "description": "Appliance with 16GB RAM and 512GB SSD",
-            "price": 2000,
-            "img_url": "https://shakill23.github.io/allImages/images/expressoMachine.jpg"
+            id: 5,
+            productName: "Expresso Machine",
+            category: "Appliance",
+            description: "Appliance with 16GB RAM and 512GB SSD",
+            price: 2000,
+            img_url: "https://shakill23.github.io/allImages/images/expressoMachine.jpg"
 
         }
-]));
+    ]));
+    function recentProducts() {
+        let arrSize = products.length
+        let latestProducts = products.reverse().slice(0, arrSize >> 1)
+        latestProducts.forEach(product => {
+            wrapper.innerHTML += `<div class="card"">
+      <img src="${product.img_url}" class="card-img-top" alt="${product.productName}" loading ="lazy">
+      <div class="card-body">
+        <h5 class="card-title">${product.productName}</h5>
+        <p class="card-text">${product.description}</p>
+      </div>
+    </div>`
+        });
+    }
+    recentProducts()
+
