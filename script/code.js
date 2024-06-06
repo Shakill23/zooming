77a227ca8455c02f6bfcb6c4fdd53f51656ae43e
Shakeel <shakeeldvd23@gmail.com>
2024-06-06 13:52:13 +0200

    pages modified

--- script/code.js
+++ script/code.js
@@ -1,1 +1,43 @@
-// 
+// Create products and store it on the local storage
+let products = JSON.parse(localStorage.getItem('products')) ? JSON.parse(localStorage.getItem('products')) : localStorage.setItem('products', JSON.stringify([
+    {
+        "productName": "Air Fryer",
+            "category": "Appliance",
+            "description": "Appliance with 16GB RAM and 512GB SSD",
+            "price": 2000,
+            "img_url": "https://shakill23.github.io/allImages/images/airFryer.webp"
+
+        },
+        {
+        "productName": "Blender",
+            "category": "Appliance",
+            "description": "Appliance with 16GB RAM and 512GB SSD",
+            "price": 2000,
+            "img_url": "https://shakill23.github.io/allImages/images/blender.jpg"
+
+        },
+        {
+        "productName": "Kettle",
+            "category": "Appliance",
+            "description": "Appliance with 16GB RAM and 512GB SSD",
+            "price": 2000,
+            "img_url": "https://shakill23.github.io/allImages/images/kettle.webp"
+
+        },
+        {
+        "productName": "Microwave",
+            "category": "Appliance",
+            "description": "Appliance with 16GB RAM and 512GB SSD",
+            "price": 2000,
+            "img_url": "https://shakill23.github.io/allImages/images/microwave.jpg"
+
+        },
+        {
+        "productName": "Expresso Machine",
+            "category": "Appliance",
+            "description": "Appliance with 16GB RAM and 512GB SSD",
+            "price": 2000,
+            "img_url": "https://shakill23.github.io/allImages/images/expressoMachine.jpg"
+
+        }
+]));
